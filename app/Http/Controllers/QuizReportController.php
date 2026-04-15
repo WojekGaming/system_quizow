baf1047edc23ec0c23559a7e4d3f65976d49f8c6
Ardrian <adrian.chrosciel@gmail.com>
2026-04-15 20:15:24 +0200

    Add quiz report system

--- app/Http/Controllers/QuizReportController.php
+++ app/Http/Controllers/QuizReportController.php
@@ -9,6 +9,13 @@ class QuizReportController extends Controller
 {
     public function store(Request $request, $id)
     {
-        dd('HIT REPORT ROUTE', $id);
+        QuizReport::create([
+            'quiz_id' => $id,
+            'reported_by_user_id' => auth()->id(),
+            'reason' => $request->reason ?? null,
+            'status' => 'new'
+        ]);
+
+        return back()->with('success', 'Zgłoszono quiz');
     }
 }
